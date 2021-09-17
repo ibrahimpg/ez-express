6a580c2a5fc61b10dddec6da3ebba49dca85887a
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2021-09-17 15:51:30 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const api = require('./api/routes');
+
+app.use(express.json());
+
+app.use('/api', api);
+
+app.listen(process.env.port);
